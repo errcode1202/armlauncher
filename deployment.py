dbbee833872a1baaf38b98ba770297599ac8cfd3
Dylan Rathbone <drathbone@atlassian.com>
2021-06-14 15:32:28 +1000

    Initial commit

---
+++ deployment.py
@@ -0,0 +1,169 @@
+import os
+from datetime import datetime, timedelta
+
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.storage import StorageManagementClient
+from azure.storage.blob import generate_account_sas, ResourceTypes, AccountSasPermissions, PublicAccess, \
+    BlobServiceClient, AccessPolicy, ContainerSasPermissions
+import json
+from azure.mgmt.resource.resources.models import DeploymentProperties
+from azure.mgmt.resource.resources.models import Deployment
+
+credential = AzureCliCredential()
+subscription_id = "a6864bfe-c3fd-4771-a921-616ed4c2cb0a"
+storage_client = StorageManagementClient(credential, subscription_id)
+resource_client = ResourceManagementClient(credential, subscription_id)
+
+
+def provision_resource_group(resource_group_name, region):
+    rg_result = resource_client.resource_groups.create_or_update(
+        f"{resource_group_name}",
+        {
+            "location": f"{region}"
+        }
+    )
+
+    print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
+
+
+def create_storage_account(resource_group_name, region):
+    storage_account_name = f"{resource_group_name}storage"
+    availability_result = storage_client.storage_accounts.check_name_availability(
+        {"name": storage_account_name}
+    )
+
+    if not availability_result.name_available:
+        print(availability_result.message)
+        print(f"Storage name {storage_account_name} is already in use. Try another name.")
+        exit()
+
+    # The name is available, so provision the account
+    poller = storage_client.storage_accounts.begin_create(resource_group_name, storage_account_name,
+                                                          {
+                                                              "location": f"{region}",
+                                                              "kind": "StorageV2",
+                                                              "sku": {"name": "Standard_LRS"}
+                                                          }
+                                                          )
+
+    # Long-running operations return a poller object; calling poller.result()
+    # waits for completion.
+    account_result = poller.result()
+    print(f"Provisioned storage account {account_result.name}")
+
+
+def create_blob(resource_group_name):
+    storage_account_name = f"{resource_group_name}storage"
+    keys = storage_client.storage_accounts.list_keys(resource_group_name, storage_account_name)
+
+    print(f"Primary key for storage account: {keys.keys[0].value}")
+
+    conn_string = f"DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName={storage_account_name};AccountKey={keys.keys[0].value}"
+
+    print(f"Connection string: {conn_string}")
+
+    # Step 4: Provision the blob container in the account (this call is synchronous)
+    blob_name = f"{storage_account_name}blob"
+    container = storage_client.blob_containers.create(resource_group_name, storage_account_name, blob_name, {})
+
+    # The fourth argument is a required BlobContainer object, but because we don't need any
+    # special values there, so we just pass empty JSON.
+
+    print(f"Provisioned blob container {container.name}")
+
+
+def upload(resource_group_name, product, dest):
+    source = f"/Users/drathbone/Git/azure/atlassian-azure-deployment/{product}"
+    if os.path.isdir(source):
+        upload_dir(resource_group_name, source, dest)
+    else:
+        upload_file(resource_group_name, source, dest)
+
+
+def upload_file(resource_group_name, source, dest):
+    storage_account_name = f"{resource_group_name}storage"
+    keys = storage_client.storage_accounts.list_keys(resource_group_name, storage_account_name)
+    conn_string = f"DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName={storage_account_name};AccountKey={keys.keys[0].value}"
+
+    blob_service_client = BlobServiceClient.from_connection_string(conn_string)
+
+    blob_name = f"{storage_account_name}blob"
+
+    client = blob_service_client.get_container_client(blob_name)
+
+    print(f'Uploading {source} to {dest}')
+    with open(source, 'rb') as data:
+        client.upload_blob(name=dest, data=data)
+
+
+def upload_dir(resource_group_name, source, dest):
+    prefix = '' if dest == '' else dest + '/'
+    prefix += os.path.basename(source) + '/'
+    for root, dirs, files in os.walk(source):
+        for name in files:
+            dir_part = os.path.relpath(root, source)
+            dir_part = '' if dir_part == '.' else dir_part + '/'
+            file_path = os.path.join(root, name)
+            blob_path = prefix + dir_part + name
+            upload_file(resource_group_name, file_path, blob_path)
+
+
+def get_and_set_container_access_policy(resource_group_name):
+    storage_account_name = f"{resource_group_name}storage"
+    keys = storage_client.storage_accounts.list_keys(resource_group_name, storage_account_name)
+    conn_string = f"DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName={storage_account_name};AccountKey={keys.keys[0].value}"
+    service_client = BlobServiceClient.from_connection_string(conn_string)
+    container_client = service_client.get_container_client(f"{storage_account_name}blob")
+
+    # Create access policy
+    access_policy = AccessPolicy(permission=ContainerSasPermissions(read=True, write=True),
+                                 expiry=datetime.utcnow() + timedelta(hours=1),
+                                 start=datetime.utcnow() - timedelta(minutes=1))
+
+    identifiers = {'read': access_policy}
+
+    # Specifies full public read access for container and blob data.
+    public_access = PublicAccess.Container
+
+    # Set the access policy on the container
+    container_client.set_container_access_policy(signed_identifiers=identifiers, public_access=public_access)
+
+
+def deploy(resource_group_name, product):
+    get_and_set_container_access_policy(resource_group_name)
+    storage_account_name = f"{resource_group_name}storage"
+    blob_name = f"{storage_account_name}blob"
+    url = f"https://{storage_account_name}.blob.core.windows.net/{blob_name}/{product}/nestedtemplates"
+
+    template_path = "/Users/drathbone/Git/azure/atlassian-azure-deployment/crowd/mainTemplate.json"
+    with open(template_path, 'r') as template_file_fd:
+        template = json.load(template_file_fd)
+
+    parameters = {
+        'crowdClusterSize': "trial",
+        '_artifactsLocation': url,
+        'sshKey': "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHjWkyH4khcIlwwPLjEF7v+3IhUK7ucH3DFjcyyxJdTFyY4UKjB+At1aGkcq6t6VLlHCddY5d8++u8pc3Ky6pZhJhygfJ7n9rtHkJDV4EJGff+BKp3B1R58ryoD4kslWJLFQYQg1zQKK8xIDZfjrZt75AkNK4y/FmypCsPDNIYsr5qw2hnXNX/gZL/Zhr44gvNoj0aUJJSBOZ/LEslxnEr2YaqjBY3EmapO4K9YAFYZzgqXhh3awonv4vt5dHH2WO581+TKIf+UYNOxRq0H5gxkYA83PC/uhNp2keBxva0HXhGl5YXQjGqODlMy1Wj12DLhTXAM9JvZqUHaMafilIUU7qjgvsY4WUrFYvcNcye5N1WUjri60WSPDKrhsgTF0/qWm/1uICoirG0ajFAopuREtUzjQjaM/BdXzWuRywHB8Mh0jbXgaziFj4ojIGTCqyn+X7UalZs9bYsoE6BzVB1ZI1JDvN6aIdiJJyJrcTlO/PkWUtBa5F3NkBkoQuJ0pEszfG48aWrWDBpxN608dwOUdbHmJNKsRT3aZ83dfcHOKd3mshwb8E0hODTUDFHLaHxu7hOLJm2C13BL9+ishPIHx9bf/wP7RLK7RJ3SoKUJsP5e7l5v+jdoHM8sxbQYWpSnnGU8L1DtlpXlfTwy2ZF9xYdFlFyemVzwqG31f6plw== drathbone@atlassian.com",
+        'sshUserName': "crowdadmin",
+        'location': "northeurope",
+        'dbPassword': ".Jkv435jxaDKL2345KA7YpbLyWJLPmocWx43rcn69",
+        'enableEmailAlerts': False,
+        'enableApplicationInsights': False,
+        'enableAnalytics': False
+    }
+    parameters = {k: {'value': v} for k, v in parameters.items()}
+
+    properties = DeploymentProperties(
+        mode="incremental",
+        template=template,
+        parameters=parameters
+    )
+
+    deploy_parameter = Deployment(properties=properties)
+
+    deployment_async_operation = resource_client.deployments.begin_create_or_update(
+        resource_group_name,
+        f"{product}-deployment",
+        deploy_parameter
+    )
+    deployment_async_operation.wait()
---
+++ main.py
@@ -0,0 +1,26 @@
+# https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-example-storage?tabs=cmd#3-write-code-to-provision-storage-resources
+# https://github.com/Azure/azure-sdk-for-python/blob/master/sdk/storage/azure-storage-blob/
+# https://github.com/Azure-Samples/resource-manager-python-template-deployment
+# https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python#upload-blobs-to-a-container
+
+from deployment import provision_resource_group
+from deployment import create_storage_account
+from deployment import create_blob
+from deployment import upload
+from deployment import deploy
+
+
+def create_stack(name, location):
+    provision_resource_group(name, location)
+    create_storage_account(name, location)
+    create_blob(name)
+    upload(name, product, "")
+    deploy(name, product)
+
+
+if __name__ == '__main__':
+    # Capture details
+    resource_group_name = input("Provide a name for the resource group: ")
+    region = input("Region to deploy to: ")
+    product = input("Product to deploy: ")
+    create_stack(resource_group_name, region)
